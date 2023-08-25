b3b981000bf4e1389b6e68f383bcece1f7bb41bb
Abbas Ali <abbas.taxi.25.1@mountblue.tech>
2023-08-25 13:54:41 +0530

    create match card game functionality

--- script.js
+++ script.js
@@ -1,67 +1,122 @@
-const gameContainer = document.getElementsById("game");
+const gameContainer = document.getElementById("game")
 
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+]
+
+let cardFlipped = false
+let lockGame = false
+let firstCard, secondCard
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
 // it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
-  let counter = array.length;
+    let counter = array.length
 
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    let index = Math.floor(Math.random() * counter);
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter)
 
-    // Decrease counter by 1
-    counter--;
+        // Decrease counter by 1
+        counter--
 
-    // And swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
+        // And swap the last element with it
+        let temp = array[counter]
+        array[counter] = array[index]
+        array[index] = temp
+    }
 
-  return array;
+    return array
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuffledColors = shuffle(COLORS)
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
-    const newDiv = document.createElement("div");
+    for (let color of colorArray) {
+        // create a new div
+        const newDiv = document.createElement("div")
 
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
+        // give it a class attribute for the value we are looping over
+        newDiv.classList.add(color)
 
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
+        // call a function handleCardClick when a div is clicked on
+        newDiv.addEventListener("click", handleCardClick)
 
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
-  }
+        // append the div to the element with an id of game
+        gameContainer.append(newDiv)
+    }
 }
 
 // TODO: Implement this function!
 function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you clicked",event.target);
+    // you can use event.target to see which element was clicked
+    // console.log("you clicked", event.target)
+    if (lockGame) return
+    if (event.target === firstCard) return
+
+    event.target.classList.add("flip")
+    event.target.style.backgroundColor = event.target.classList[0]
+
+    if (!cardFlipped) {
+        // first click
+        cardFlipped = event.target
+        firstCard = event.target
+
+        return
+    }
+
+    // second click
+    secondCard = event.target
+
+    checkForMatch()
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.classList[0] === secondCard.classList[0]
+
+    isMatch ? lockCards() : unflipCards()
+}
+
+function lockCards() {
+    firstCard.removeEventListener("click", handleCardClick)
+    secondCard.removeEventListener("click", handleCardClick)
+
+    resetBoard()
+}
+
+function unflipCards() {
+    lockGame = true
+
+    setTimeout(() => {
+        firstCard.classList.remove("flip")
+        secondCard.classList.remove("flip")
+        firstCard.style.backgroundColor = ""
+        secondCard.style.backgroundColor = ""
+
+        resetBoard()
+    }, 1000)
+}
+
+function resetBoard() {
+    cardFlipped = false
+    lockGame = false
+    firstCard = null
+    secondCard = null
 }
 
 // when the DOM loads
-createDivsForColors(shuffledColors);
+createDivsForColors(shuffledColors)
